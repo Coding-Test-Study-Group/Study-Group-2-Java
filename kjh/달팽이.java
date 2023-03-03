ef56043e3f26bd8f9f09356bb461f464284c433b
jhva <junghun8158@naver.com>
2023-03-03 22:45:45 +0900

    김정훈: 가장흔한단어찾기,달팽이,주식가격

---
+++ kjh/달팽이.java
@@ -0,0 +1,20 @@
+package org.example.homework;
+
+import java.util.Arrays;
+
+public class 달팽이 {
+    public static void main(String[] args) {
+        solve();
+    }
+
+    static void solve() {
+
+        int[][] arr = new int[5][5];
+
+
+        /**
+         *  빡대가리라 못하겠네요 달팽ㅇ ㅣ
+         */
+
+    }
+}
