9decbe4edc5aebfef5fa4efc17e9d2bad9dfd632
lango <xmun777@naver.com>
2023-03-20 20:26:00 +0900

    문태준: 안테나

---
+++ taejun/week3/안테나.java
@@ -0,0 +1,33 @@
+package taejun.week3;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int N = Integer.parseInt(br.readLine());
+        int[] arr = new int[N];
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for(int i=0; i<N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+        
+        Arrays.sort(arr);
+
+        if(N % 2 == 0) bw.write(arr[N/2-1]+"\n");
+        else bw.write(arr[N/2]+"\n");
+
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
